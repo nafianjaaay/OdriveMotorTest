40ac18e01b69c51a4c6ba95fb9f94ece1a47c104
Nafian Falah Akbar <92433261+nafianjaaay@users.noreply.github.com>
2023-06-07 17:58:20 +0700

    Update OdriveTEST.py

--- OdriveTEST.py
+++ OdriveTEST.py
@@ -2,13 +2,14 @@ import odrive
 from odrive.enums import *
 import time
 import keyboard
+import matplotlib.pyplot as plt
 
 #----------program for testing odrive---------#
 #----wait motor calibrate, it will auto run---# 
 
 #---parameter---#
 MAX_SPEED = 100
-KP_VALUE  = 30
+KP_VALUE  = 5
 KI_VALUE  = 0.25
 BANDWIDTH = 2500
 RAMP_RATE = 100
@@ -18,6 +19,7 @@ POS_GAIN = 35
 print("finding odrive...")
 address_motor = odrive.find_any()
 print("odrive found! reset position...")
+odrive.utils.start_liveplotter(lambda:[address_motor.axis0.encoder.shadow_count])
 address_motor.axis0.motor.config.current_control_bandwidth = BANDWIDTH
 address_motor.axis0.controller.config.vel_gain = KP_VALUE
 address_motor.axis0.controller.config.vel_integrator_gain = KI_VALUE 
@@ -31,10 +33,9 @@ print("changing mode...")
 address_motor.axis0.controller.config.input_mode = InputMode.VEL_RAMP
 address_motor.axis0.controller.config.control_mode = ControlMode.VELOCITY_CONTROL
 address_motor.axis0.requested_state = AxisState.CLOSED_LOOP_CONTROL
-time.sleep(2)
+time.sleep(1)
 print("SETUP DONE!")
 time.sleep(1)
-start_liveplotter(lambda:[address_motor.axis0.encoder.shadow_count])
 
 #--starting speed--#
 velocity_motor = 10
@@ -44,24 +45,24 @@ while True:
     if keyboard.is_pressed("h"):    # PRESS KEYBOARD BUTTON 'h' FOR TURNING OFF ODRIVE MOTOR
         velocity_motor = 0
     elif keyboard.is_pressed("p"):  # PRESS KEYBOARD BUTTON 'p' FOR TURNING ON ODRIVE MOTOR CW
-        velocity_motor = 75
+        velocity_motor = 50
     elif keyboard.is_pressed("o"):  # PRESS KEYBOARD BUTTON 'o' FOR TURNING ON ODRIVE MOTOR CCW
-        velocity_motor = -75
+        velocity_motor = -50
     elif keyboard.is_pressed("l"):  # PRESS KEYBOARD BUTTON 'l' FOR TURNING ON ODRIVE MOTOR CW SEQUENTIAL
         velocity_motor = 0
-        while velocity_motor < 100:
+        while velocity_motor < 50:
             velocity_motor += 1
             time.sleep(1)
     elif keyboard.is_pressed("k"):  # PRESS KEYBOARD BUTTON 'k' FOR TURNING ON ODRIVE MOTOR CCW SEQUENTIAL
         velocity_motor = 0
-        while velocity_motor > -100:
+        while velocity_motor > -50:
             velocity_motor -= 1
             time.sleep(1)
 
     if address_motor.axis0.current_state == 1:
-        print("RESET MOTOR ERROR...")
+        print("error detected!!! reseting motor...")
         address_motor.clear_errors()
         address_motor.axis0.requested_state = 8
     else:
-        print("STARTING MOTOR...")
+        print("starting motor...")
         address_motor.axis0.controller.input_vel = velocity_motor
